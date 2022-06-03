27464e95a3707c40bcbd5bd491898cdee2666bc2
Jennifer Gifford <jgifford82@gmail.com>
2022-06-02 20:27:36 -0600

    commit tree 960a750a43e86d55452e18e5dee10bd8ada5fc13

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
+
+
+// const input = document.getElementById('input');
+
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('Hee hee, that tickles!');
+// }
+
+// input.addEventListener('click', clickAlert);
